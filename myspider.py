59ee609eb8265cf11cecd0c07178654f954b9fd4
juan carlos <hhjuan27@hotmail.com>
2019-05-31 23:21:28 -0500

    parsenado pagina de ventas

--- myspider.py
+++ myspider.py
@@ -3,9 +3,7 @@ import scrapy
 class BlogSpider(scrapy.Spider):
     name = 'mercadolibre'
     start_urls = ['https://www.mercadolibre.com.mx']
-
     def parse(self, response):
-        
         for title in response.css('.ui-item__content'):
             yield {'title': title.css('.ui-item__title ::text').get()}
         for next_page in response.css('a.andes-pagination__link prefetch'):
